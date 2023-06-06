461e64b4c92e11f5cc000b4ee80ac6cf9e6baae0
20wh1a1254 <93141159+20wh1a1254@users.noreply.github.com>
2023-06-06 07:49:09 +0530

    usercard2

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("Hi I'm Susmitha");
+var data1={image : "abc.jpg",name : " Susmitha Reddy Banda", designation:"Student"};
+var data2={image : "edf.jpg",name : "B.Susmitha Reddy", designation:"student"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
